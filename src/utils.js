768e5d70e3d849e31be86ae1b8fa404694971417
Guillermo Cely <megaguillermo@gmail.com>
2026-08-07 12:11:54 -0500

    feat: agregar fecha además de hora en los mensajes del chat

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
 export function formatTime(date = new Date()) {
-  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  return date.toLocaleString([], { 
+    day: '2-digit', 
+    month: '2-digit', 
+    year: 'numeric',
+    hour: '2-digit', 
+    minute: '2-digit' 
+  });
 }
 
 export function buildMessages(history, character) {
